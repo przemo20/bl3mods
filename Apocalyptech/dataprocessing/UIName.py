ca4943b0da19c7e1ed89c442fd1fd4835d3a7357
przemo20 <razer.nostale@gmail.com>
2024-12-17 16:19:57 +0100

    Fix for Lorelei's name showing only as “Unknown”

--- Apocalyptech/dataprocessing/UIName.py
+++ Apocalyptech/dataprocessing/UIName.py
@@ -19,20 +19,20 @@ PathList_DNT = list(data.find('', 'DNT'))
 
 print('> DialogEnumValue <')
 for a in sorted(PathList_DialogEnumValue):
-    DialogEnumValueData = data.get_exports(a, "DialogEnumValue")
+    DialogEnumValueData = data.get_exports(a, 'DialogEnumValue')
     for b in DialogEnumValueData:
         try:
-            UINamePath = b["UIName"][1]
-            # print(UINamePath)
-            SpeakerUIName = data.get_exports(UINamePath, "GbxUIName")[0]["DisplayName"]["string"]
-            UINameDictionary_DialogEnumValue.setdefault(b["_jwp_object_name"], SpeakerUIName)  # .append(SpeakerUIName)
+            UINamePath = b['UIName'][1]
+            # print('UINamePath: {}'.format(UINamePath))
+            SpeakerUIName = data.get_exports(UINamePath, 'GbxUIName')[0]['DisplayName']['string']
+            UINameDictionary_DialogEnumValue.setdefault(b['_jwp_object_name'], SpeakerUIName)
         except KeyError as e:
-            # print(f"*** {e.args[0]} key for {a} is missing! ***")
+            # print(f'*** {e.args[0]} key for {a} is missing! ***')
             pass
 
 for x in sorted(UINameDictionary_DialogEnumValue):
-    # print("{}: {}".format(x, UINameDictionary_DialogEnumValue[x]))
-    print("\'{}\': \'{}\',".format(x, UINameDictionary_DialogEnumValue[x].replace('\'', '\\\'')))
+    # print('{}: {}'.format(x, UINameDictionary_DialogEnumValue[x]))
+    print('\'{}\': \'{}\','.format(x, UINameDictionary_DialogEnumValue[x].replace('\'', '\\\'')))
 
 
 print()
@@ -43,33 +43,33 @@ print()
 ### DialogNameTag (DNT) files ###
 
 print('> DialogNameTag <')
-for a in sorted(PathList_DNT):  # sorted(NameTagList)
+for a in sorted(PathList_DNT):
     DialogNameTagData = data.get_data(a)
     NameTagEnumValue = None
     SpeakerUIName = None
     try:
         for b in DialogNameTagData:
-            if b["export_type"] == "DialogNameTag":
-                NameTagEnumValue = b["NameTagEnumValue"][0]
-                # print(NameTagEnumValue)
+            if b['export_type'] == 'DialogNameTag':
+                NameTagEnumValue = b['NameTagEnumValue'][0]
+                # print('NameTagEnumValue: {}'.format(NameTagEnumValue))
         for c in DialogNameTagData:
-            if c["export_type"] == "CharacterEchoData":
-                SpeakerUINamePath = c["SpeakerUIName"][1]
-                # print(SpeakerUINamePath)
-                SpeakerUIName = data.get_exports(SpeakerUINamePath, "GbxUIName")[0]["DisplayName"]["string"]
-                # print(SpeakerUIName)
-        if NameTagEnumValue != None and SpeakerUIName != None:
-            flag = False
-            for t in UINameDictionary_DNT:
-                if t == NameTagEnumValue:
-                    for h in UINameDictionary_DNT[t]:
-                        if h == SpeakerUIName:
-                            flag = True
-            if flag == False:
-                UINameDictionary_DNT.setdefault(NameTagEnumValue, []).append(SpeakerUIName)
+            if c['export_type'] == 'CharacterEchoData':
+                SpeakerUINamePath = c['SpeakerUIName'][1]
+                # print('SpeakerUINamePath: {}'.format(SpeakerUINamePath))
+                SpeakerUIName = data.get_exports(SpeakerUINamePath, 'GbxUIName')[0]['DisplayName']['string']
+                # print('SpeakerUIName: {}'.format(SpeakerUIName))
+                if NameTagEnumValue != None and SpeakerUIName != None:
+                    flag = False
+                    for t in UINameDictionary_DNT:
+                        if t == NameTagEnumValue:
+                            for h in UINameDictionary_DNT[t]:
+                                if h == SpeakerUIName:
+                                    flag = True
+                    if flag == False:  # To avoid adding duplicate values to the dictionary
+                        UINameDictionary_DNT.setdefault(NameTagEnumValue, []).append(SpeakerUIName)
     except:
         pass
 
 for x in sorted(UINameDictionary_DNT):
-    print("\'{}\': {},".format(x, UINameDictionary_DNT[x]))
-    # print("\"{}\": \"{}\",".format(x, UINameDictionary_DNT[x]))
+    # print('{}: {}'.format(x, UINameDictionary_DNT[x]))
+    print('\'{}\': {},'.format(x, UINameDictionary_DNT[x]))
